68fefd57c89886ccecafd67c14c7e2b07ea6d6d0
Ion Utale <utale.ion@gmail.com>
2026-06-03 13:34:41 +0200

    feat: show page icon in nested PageBlock component

--- web/src/lib/components/blocks/PageBlock.svelte
+++ web/src/lib/components/blocks/PageBlock.svelte
@@ -29,8 +29,16 @@
   tabindex="0"
   role="button"
 >
-  <svg class="w-4 h-4 text-base-content/40 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-  </svg>
+  {#if block?.content?.icon}
+    {#if block.content.icon_type === 'image'}
+      <img src={block.content.icon} alt="" class="w-4 h-4 rounded object-cover shrink-0" />
+    {:else}
+      <span class="text-sm shrink-0">{block.content.icon}</span>
+    {/if}
+  {:else}
+    <svg class="w-4 h-4 text-base-content/40 shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+    </svg>
+  {/if}
   <span class="text-sm">{title}</span>
 </div>
